e70b1ec29e7bd331f780e027602af04238449f67
RomanBratus <romanbratus@gmail.com>
2023-05-01 22:54:46 +0300

    update all

---
+++ src/components/ContactItem/ContactItem.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ContactItem = ({ contacts, deleteContactOn }) => {
+  return contacts.map(({ name, number, id }) => (
+    <li
+      style={{
+        display: 'flex',
+        padding: '0',
+        marginTop: '20px',
+        justifyContent: 'left',
+      }}
+      key={id}
+    >
+      {name} : <span>{number}</span>
+      <button
+        style={{
+          boxSizing: 'border-box',
+          width: '100px',
+          height: '30px',
+          background: '#808e9e',
+          border: 'none',
+          cursor: 'pointer',
+          borderRadius: '6px',
+          marginLeft: '15px',
+        }}
+        type="button"
+        onClick={() => {
+          deleteContactOn(id);
+        }}
+      >
+        Delete
+      </button>
+    </li>
+  ));
+};
+
+ContactItem.propTypes = {
+
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+
+  onDeleteContact: PropTypes.func,
+};
+
+export default ContactItem;
---
+++ src/components/Finder/Finder.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Finder = ({ filter, onChange }) => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'left',
+        alignItems: 'center',
+      }}
+    >
+      <label>Find contact by name</label>
+      <input
+        style={{
+          height: '34px',
+          background: '#ccd6e3',
+          borderRadius: '6px',
+          marginLeft: '10px',
+        }}
+        type="text"
+        value={filter}
+        onChange={onChange}
+      ></input>
+    </div>
+  );
+};
+Finder.propTypes = {
+  filter: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+export default Finder;
